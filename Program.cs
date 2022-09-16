51e6e7fa11224d656c98752372eb2ed85b72dc59
ElenaMalysheva <photo.malysheva@gmail.com>
2022-09-16 20:28:50 +0300

    HW done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void NewArray(string[] array)
+{
+    string[] newArray = new string[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (IsLessThree(array[i]))
+        newArray[i] = array[i];
+    }
+    PrintArray(newArray);
+
+}
+
+bool IsLessThree (string str)
+{
+    int n = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        n++;
+    }
+    if (n <=3 )
+    {
+        return true;
+    }
+    else return false;
+}
+
+string[] startArray = {"hello", "2",":-P"};
+
+Console.WriteLine("Исходный массив: ");
+PrintArray(startArray);
+Console.WriteLine("Новый массив: ");
+NewArray(startArray);
